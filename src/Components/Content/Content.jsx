369a2af5012c8a3e44ecb219a23a0fb2a7a03f78
Kemény Vince <kemeny.vince@outlook.hu>
2024-02-11 14:56:57 +0100

    Refactor Content and ContentCard components
    The code changes aim to enhance data handling and component structure in Content and ContentCard. The props received by ContentCard have been destructured for better accessibility. Also, a new ContentBody class has been introduced in the Content component to organize data. Display structure has been updated to include more fields inside the ContentCard component.

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,20 +1,59 @@
 // This is a so-called article place and also the body of the page!
 // This will use the content cards as well
 import {ContentCard} from "./ContentCard/ContentCard";
+import {type} from "@testing-library/user-event/dist/type";
+
 
 export function Content() {
 	// To make things easier, I used structs to display content's text
-	const school0 = {
-		type: "school",
-		name: "Vajk-Sziget Elementary School",
-		startDate: 2009,
-		endDate: 2017,
-		description: "",
+
+	class ContentBody {
+		_name =""
+		_type = ""
+		_start = 0
+		_end = 0
+		_description = ""
+
+		constructor(name, type, start, end, description) {
+			this._name = name
+			this._type = type
+			this._start = start
+			this._end = end
+			this._description = description
+		}
+
+		getName = () => this._name
+		getType = () => this._type
+		getStart = () => this._start
+		getEnd = () => this._end
+		getDescription = () => this._description
 	}
 
+	const school0 = new ContentBody("Vajk-Sziget Elementary School", "school", 2009, 2017, "")
+
+	const school1 = new ContentBody("BMSC Trefort Ágoston Bilingual Technical School Budapest", "school", 2017, 2022, "Graduation")
+
+	const school2 = new ContentBody("BGSC Pestszentlőrinci Technical School", "school", 2022, 2023, "Software" +
+		" development and testing certificate")
+
+
+	const workplace0 = {
+		type: ""
+	}
+
+	const workplace1 = {
+
+	}
+
+	const workplace2 = {
+
+	}
+
+	const cards = [school0, school1, school2, workplace0, workplace1, workplace2]
+
 	return (
-		<>
-		<ContentCard source={school0}/>
-		</>
+		<div className={"frappe"}>
+			<ContentCard props={school0}/>
+		</div>
 	)
 }
--- src/Components/Content/ContentCard/ContentCard.jsx
+++ src/Components/Content/ContentCard/ContentCard.jsx
@@ -1,11 +1,11 @@
-export function ContentCard(props) {
-
+export function ContentCard({props}) {
+	const [index, name, type, start, end, description] = props
 	return (
 		<>
-			<div>
-				<h3>{props.source.name}</h3>
-				<div></div>
-				<div></div>
+			<div id={index}>
+				<h4>{name}</h4>
+				<div>{start} - {end}</div>
+				<div>{description}</div>
 			</div>
 		</>
 	)
