b4e579156256c9a71a7d7fb3d8f9bee8246dded7
Kemény Vince <kemeny.vince@outlook.hu>
2024-02-11 15:27:23 +0100

    Update Header component layout and style
    This commit updates the Header component layout and visual styling for readability and better user interface. It has improved the arrangement and formatting of elements within the header, giving a better representation of user profile and contact information. Plus, styling attributes have been applied to enhance elements localization and display.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,28 +1,29 @@
-import {ExampleLoaderComponent} from "../../dev/palette";
 import profilepic from "./rework profilkép.jpg"
 
 export function Header() {
 	return (
-		<header className="frappe ctp-frappe">
-			<div>
-			<h1 className="font-bold ctp-teal">
-				Kemény Vince
-			</h1>
-			</div>
-			<div>
-			<img alt="My Profile picture" src={profilepic}/>
-			</div>
-			<div>
-				<div>
-					<address>
-						Email address<br/>
-						<a href="mailto:kemeny.vince@outlook.hu" id="email">kemeny.vince@outlook.hu</a><br/>
-						Phone number<br/>
-						<a href="tel:+36303909201" id="tel">+36303909201</a><br/>
-						<a href="https://github.com/KVince002" id="github">My GitHub page</a><br/>
-						<a href="https://gitlab.com/Cincennes" id="gilab">My Gitlab page</a>
-					</address>
+		<header className="frappe ctp-frappe flex-auto bg-gradient-to-r from-ctp-green to-ctp-teal flex-row">
+			<div className="flex flex-row place-content-evenly">
+				<div className="flex items-stretch content-center">
+					<h1 className="font-extrabold text-2xl justify-items-stretch content-center">Kemény Vince</h1>
 				</div>
+				<div className="flex items-stretch">
+					<img alt="My Profile picture" src={profilepic} className="rounded-full mx-auto size-24"/>
+				</div>
+			</div>
+			<div>
+				<address className="flex flex-row justify-evenly">
+					<div>
+						<p className="font-bold">Email address</p>
+						<a href="mailto:kemeny.vince@outlook.hu" id="email">kemeny.vince@outlook.hu</a>
+					</div>
+					<div>
+						<p className="font-bold">Phone number</p>
+						<a href="tel:+36303909201" id="tel">+36303909201</a>
+					</div>
+					<a className="font-semibold" href="https://github.com/KVince002" id="github">My GitHub page</a><br/>
+					<a className="font-semibold" href="https://gitlab.com/Cincennes" id="gilab">My Gitlab page</a>
+				</address>
 			</div>
 		</header>
 	)
